46a7b4d575124a8e42e10f78a2bbd6d9e983d102
mojtaba <mojtaba@shuwela.com>
2025-12-04 21:50:05 +0330

    added url to portfolio

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -728,6 +728,12 @@ export interface ApiPortfolioPortfolio extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    url: Schema.Attribute.String &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
   };
 }
 
